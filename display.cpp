06964e5c766a49055eb59592af8d29b33580ef9f
Baibhav Chakraborty <baibhav@ialoy.com>
2020-10-12 09:54:38 +0530

    Made some other changes in display.cpp file
    Signed-off-by: Baibhav Chakraborty <baibhav@ialoy.com>

--- display.cpp
+++ display.cpp
@@ -7,7 +7,7 @@ void display(MYSQL *conn)
 {
 	MYSQL_ROW row;
 	MYSQL_RES *res_set;
-	cout<<"Item_ID"<<" "<<"Item_Name"<<" "<<"Quantity"<<" "<<"Unit"<<" "<<"Category"<<" "<<"Item_Price"<<endl;
+	cout<<"Item_ID"<<" | "<<"Item_Name"<<" | "<<"Quantity"<<" | "<<"Unit"<<" | "<<"Category"<<" | "<<"Item_Price"<<endl;
 	mysql_query(conn,"SELECT * FROM Item;");
 	res_set=mysql_store_result(conn);
 	while((row=mysql_fetch_row(res_set))!=NULL)
@@ -22,9 +22,9 @@ void display(MYSQL *conn)
 			for(i=0;i<6;i++)
 			{
 				if(row[i]==NULL)
-					cout<<"NULL"<<"\t";
+					cout<<"NULL"<<"\t|";
 				else
-					cout<<row[i]<<"\t";
+					cout<<row[i]<<"\t|";
 			}
 		}
 		cout<<"\n";
